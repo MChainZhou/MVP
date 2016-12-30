c73f947f2618bdfbf20a7aad2da5aeb2141c7b9e
apple <apple@appledeMacBook-Pro-4.local>
2016-12-30 11:53:11 +0800

    创建项目

---
+++ MVP/MVP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVP
+//
+//  Created by apple on 2016/12/30.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
